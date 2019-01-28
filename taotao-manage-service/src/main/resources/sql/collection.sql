964722c231a8196f1b239219cc8a401cea5e9857
Spencer Reid <304552301@qq.com>
2019-01-28 21:52:26 +0800

    sql script

---
+++ taotao-manage-service/src/main/resources/sql/collection.sql
@@ -0,0 +1,1 @@
+INSERT INTO `collection` VALUES ('75c749d6-1c65-44c0-b11b-a7a1a04eb493', 'c0b49971-a2a2-4834-b0cc-01cf0b03878b', '1');
---
+++ taotao-manage-service/src/main/resources/sql/user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` VALUES (1, 'admin', 'admin', 'Supreme', '1', '15810206547');
